33671c3c1b61bd881eeaffd08f6a37f458a0a64f
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2024-11-02 16:41:44 -0300

    fixed structure project

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,7 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vinicius457/BancoDeDados2/config"
+	"gorm.io/gorm"
 )
+
+var (
+	db *gorm.DB
+    logger *config.Logger
+)
+
+func InitializingHandlers() {
+	logger = config.GetLogger("handler")
+    db = config.GetDbConnection()
+}
 
 func HelloCRUD(ctx *gin.Context){
 	ctx.JSON(http.StatusOK, gin.H{
